bed1826c1be94ec92bc83f4ef73c8dea9f79041b
maksimjeet20566 <98419737+MakC-Ukr@users.noreply.github.com>
2022-07-30 15:38:46 +0530

    fix: L1-bot

--- L1-bot/src/agent.ts
+++ L1-bot/src/agent.ts
@@ -7,84 +7,94 @@ import {
   FindingType,
 } from "forta-agent";
 var ethers = require("ethers");
-import { DAI_L1_ADDRESS, ERC20_ABI, L1_ESCROW_ADDRESS_ARB, L1_ESCROW_ADDRESS_OP, API_URL, QUERY_OP, QUERY_ARB, HEADERS} from "./constants";
+import {
+  DAI_L1_ADDRESS,
+  ERC20_ABI,
+  L1_ESCROW_ADDRESS_ARB,
+  L1_ESCROW_ADDRESS_OP,
+  API_URL,
+  QUERY_OP,
+  QUERY_ARB,
+  HEADERS,
+} from "./constants";
 import axios from "axios";
 
-
-
 export function provideHandleBlock(
-      DaiL1Address: string,
-      erc20Abi: any[], 
-      l1EscrowAddressOp: string,
-      l1EscrowAddressArb : string,
-      apiUrl : string,
-      queryOp: string, 
-      queryArb: string, 
-      headers : {}
-  ): HandleBlock {
-
-
+  DaiL1Address: string,
+  erc20Abi: any[],
+  l1EscrowAddressOp: string,
+  l1EscrowAddressArb: string,
+  apiUrl: string,
+  queryOp: string,
+  queryArb: string,
+  headers: {}
+): HandleBlock {
   const func = async (i: number) => {
     const resp = await axios.post(
       apiUrl,
       {
         query: i === 0 ? queryOp : queryArb,
       },
-      {headers: headers,}
+      { headers: headers }
     );
-    return resp['data']['data']['alerts']['alerts'][0]['metadata'];
-  }
+    return resp["data"]["data"]["alerts"]["alerts"][0]["metadata"];
+  };
 
-      
-    let provider = new ethers.providers.JsonRpcProvider(getJsonRpcUrl());
-    let DAI_L1 = new ethers.Contract(DaiL1Address, erc20Abi, provider);
+  let provider = new ethers.providers.JsonRpcProvider(getJsonRpcUrl());
+  let DAI_L1 = new ethers.Contract(DaiL1Address, erc20Abi, provider);
 
-    return async (_: BlockEvent) => {
-      const findings: Finding[] = [];
-      for(let i = 0; i < 2; i++){
-        const chainName = i === 0 ? "Optimism" : "Arbitrum";
-        let escrowBal = parseFloat(await DAI_L1.balanceOf( i === 0 ? l1EscrowAddressOp : l1EscrowAddressArb)) / Math.pow(10, 18);
-        let flag = false;
-        let L2_metadata = await func(i);
+  return async (_: BlockEvent) => {
+    const findings: Finding[] = [];
+    for (let i = 0; i < 2; i++) {
+      const chainName = i === 0 ? "Optimism" : "Arbitrum";
+      let escrowBal =
+        parseFloat(
+          await DAI_L1.balanceOf(
+            i === 0 ? l1EscrowAddressOp : l1EscrowAddressArb
+          )
+        ) / Math.pow(10, 18);
+      let flag = false;
+      let L2_metadata = await func(i);
 
-        if(parseFloat(L2_metadata['totalSupplyDai']) > escrowBal)
-          flag = true;
+      if (parseFloat(L2_metadata["totalSupplyDai"]) > escrowBal) flag = true;
 
-        if (flag) {
-          const alertName = i === 0 ? "OP: DAI L1 less than L2" : "AB: DAI L1 less than L2";
-          findings.push(
-            Finding.fromObject({
-              name: alertName,
-              description: "DAI balance of L1 escrow for " +chainName+ " DAI bridge less than DAI supply on L2",
-              alertId: "DAI_BALANCE-1",
-              severity: FindingSeverity.High,
-              type: FindingType.Exploit,
-              protocol: "MakerDAO (mainnet)",
-              metadata: {
-                escrowBal: escrowBal.toString(),
-                L2_sup : L2_metadata['totalSupplyDai'].toString(),
-                L2_chainId: L2_metadata['chainId'],
-                L2_chainName: i === 0 ? "Optimism" : "Arbitrum"
-              },
-            })
-          );
-
-
-        }
+      if (flag) {
+        const alertName =
+          i === 0 ? "OP: DAI L1 less than L2" : "AB: DAI L1 less than L2";
+        findings.push(
+          Finding.fromObject({
+            name: alertName,
+            description:
+              "DAI balance of L1 escrow for " +
+              chainName +
+              " DAI bridge less than DAI supply on L2",
+            alertId: "DAI_BALANCE-1",
+            severity: FindingSeverity.High,
+            type: FindingType.Exploit,
+            protocol: "MakerDAO (mainnet)",
+            metadata: {
+              escrowBal: escrowBal.toString(),
+              L2_sup: L2_metadata["totalSupplyDai"].toString(),
+              L2_chainId: L2_metadata["chainId"],
+              L2_chainName: i === 0 ? "Optimism" : "Arbitrum",
+            },
+          })
+        );
       }
-      return findings;
+    }
+    return findings;
   };
 }
 
 export default {
   handleBlock: provideHandleBlock(
-      DAI_L1_ADDRESS, 
-      ERC20_ABI, 
-      L1_ESCROW_ADDRESS_OP, 
-      L1_ESCROW_ADDRESS_ARB, 
-      API_URL, 
-      QUERY_OP, 
-      QUERY_ARB, 
-      HEADERS
-    ),
+    DAI_L1_ADDRESS,
+    ERC20_ABI,
+    L1_ESCROW_ADDRESS_OP,
+    L1_ESCROW_ADDRESS_ARB,
+    API_URL,
+    QUERY_OP,
+    QUERY_ARB,
+    HEADERS
+  ),
 };
--- L1-bot/src/constants.ts
+++ L1-bot/src/constants.ts
@@ -1,8 +1,10 @@
-const DAI_L1_ADDRESS : string = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
-const L1_ESCROW_ADDRESS_OP : string = "0x467194771dAe2967Aef3ECbEDD3Bf9a310C76C65";
-const L1_ESCROW_ADDRESS_ARB : string = "0xA10c7CE4b876998858b1a9E12b10092229539400";
-const API_URL : string = "https://api.forta.network/graphql";
-const ERC20_ABI : any[] = [
+const DAI_L1_ADDRESS: string = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+const L1_ESCROW_ADDRESS_OP: string =
+  "0x467194771dAe2967Aef3ECbEDD3Bf9a310C76C65";
+const L1_ESCROW_ADDRESS_ARB: string =
+  "0xA10c7CE4b876998858b1a9E12b10092229539400";
+const API_URL: string = "https://api.forta.network/graphql";
+const ERC20_ABI: any[] = [
   {
     constant: true,
     inputs: [{ internalType: "address", name: "", type: "address" }],
@@ -71,9 +73,17 @@ const QUERY_ARB: string = `query recentAlerts {
 }
 `;
 
-
-const HEADERS : {} = {
+const HEADERS: {} = {
   "content-type": "application/json",
 };
 
-export { DAI_L1_ADDRESS, ERC20_ABI, L1_ESCROW_ADDRESS_ARB, L1_ESCROW_ADDRESS_OP, QUERY_OP, QUERY_ARB, HEADERS, API_URL};
+export {
+  DAI_L1_ADDRESS,
+  ERC20_ABI,
+  L1_ESCROW_ADDRESS_ARB,
+  L1_ESCROW_ADDRESS_OP,
+  QUERY_OP,
+  QUERY_ARB,
+  HEADERS,
+  API_URL,
+};
