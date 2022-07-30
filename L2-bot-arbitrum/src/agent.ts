662e5df3f7d750cc3240720e6153d0c8bcac64bc
maksimjeet20566 <98419737+MakC-Ukr@users.noreply.github.com>
2022-07-30 13:50:58 +0530

    feat: added arb bot

---
+++ L2-bot-arbitrum/src/agent.ts
@@ -0,0 +1,45 @@
+import {
+  BlockEvent,
+  Finding,
+  HandleBlock,
+  HandleTransaction,
+  TransactionEvent,
+  FindingSeverity,
+  getJsonRpcUrl,
+  FindingType,
+} from "forta-agent";
+var ethers = require("ethers");
+import { DAI_L2_ADDRESS, ERC20_ABI, CHAIN_ID_BOT } from "./constants";
+
+export function provideHandleBlock(DaiL2Address: string, erc20Abi: any[], chainIdBot: string): HandleBlock {
+  let provider = new ethers.providers.JsonRpcProvider(getJsonRpcUrl());
+  let DAI_L2 = new ethers.Contract(DaiL2Address, erc20Abi, provider);
+
+  return async (blockEvent: BlockEvent) => {
+    let L2_totalSupply = parseFloat(await DAI_L2.totalSupply()) / Math.pow(10, 18);
+    const findings: Finding[] = [];
+    findings.push(
+      Finding.fromObject({
+        name: "DAI balance update",
+        description: `Returns the total supply of L2 Optimism DAI tokens`,
+        alertId: "OP_DAI_SUPPLY-1",
+        severity: FindingSeverity.Low,
+        type: FindingType.Info,
+        protocol: "MakerDAO",
+        metadata: {
+          blockNumber: blockEvent.blockNumber.toString(),
+          blockHash: blockEvent.blockHash,
+          chainId: chainIdBot,
+          totalSupplyDAI: L2_totalSupply.toString(),
+        },
+      })
+    );
+    return findings;
+  };
+}
+
+
+
+export default {
+  handleBlock: provideHandleBlock(DAI_L2_ADDRESS, ERC20_ABI, CHAIN_ID_BOT),
+};
