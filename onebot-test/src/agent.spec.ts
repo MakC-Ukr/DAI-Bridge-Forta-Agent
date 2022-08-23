26d8929f83ca05a2fab7e5441751ac28fd47ea69
maksimjeet20566 <98419737+MakC-Ukr@users.noreply.github.com>
2022-08-23 20:19:51 +0530

    test: test refactoring

--- onebot-test/src/agent.spec.ts
+++ onebot-test/src/agent.spec.ts
@@ -33,32 +33,6 @@ describe("DAI bridged balance checker bot tests", () => {
     mockProvider_l1 as unknown as ethers.providers.JsonRpcProvider
   );
 
-  it("doesn't return a finding on L1 when the totalSupply on L2 is less than escrow balance", async () => {
-    const TEST_BLOCK_NUMBER = 23;
-    const TEST_BLOCK_TIMESTAMP = 16523773880;
-
-    mockProvider_l1
-      .addCallTo(MOCK_DAI_L1_ADDR, TEST_BLOCK_NUMBER, iface, "totalSupply", {
-        inputs: [],
-        outputs: [L2_SUPPLY_ARR[1]],
-      })
-      .addCallTo(MOCK_DAI_L1_ADDR, TEST_BLOCK_NUMBER, iface, "balanceOf", {
-        inputs: [MOCK_L1_ESCROW_OP],
-        outputs: [L2_ESCROW_BALANCES_ARR[0]],
-      })
-      .addCallTo(MOCK_DAI_L1_ADDR, TEST_BLOCK_NUMBER, iface, "balanceOf", {
-        inputs: [MOCK_L1_ESCROW_ARB],
-        outputs: [L2_ESCROW_BALANCES_ARR[0]],
-      })
-      .setLatestBlock(TEST_BLOCK_NUMBER);
-
-    mockProvider_l1.setNetwork(1001);
-
-    const blockEvent: BlockEvent = new TestBlockEvent().setNumber(TEST_BLOCK_NUMBER).setTimestamp(TEST_BLOCK_TIMESTAMP);
-    expect(await handleBlock_l1(blockEvent)).toStrictEqual([]);
-    expect(mockFetcher.getAlertData).toHaveBeenCalled();
-  });
-
   it("returns a finding on L1 when the totalSupply on L2 is MORE than escrow balance for Arbitrum only", async () => {
     const TEST_BLOCK_NUMBER = 23;
     const TEST_BLOCK_TIMESTAMP = 16523773880;
