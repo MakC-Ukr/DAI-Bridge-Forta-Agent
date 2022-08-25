58c638f25c421050051c693ead2e64c4c95f3552
maksimjeet20566 <98419737+MakC-Ukr@users.noreply.github.com>
2022-08-25 22:46:18 +0530

    deleted unnecessary repos

--- L2-bot-optimism/src/constants.ts
+++
@@ -1,5 +0,0 @@
-const CHAIN_ID_BOT = "10";
-const DAI_L2_ADDRESS = "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1";
-const ERC20_ABI = ["function totalSupply() view returns (uint)"];
-
-export { DAI_L2_ADDRESS, ERC20_ABI, CHAIN_ID_BOT };
